fd33fc8342ad690e314e94bb5a2350a29d2aefcb
Matej Minar <matej.minar@brackets.sk>
2018-12-17 14:40:22 +0100

    fixed deprecation warning

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { createLogger } from 'redux-logger'
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 
-import rootReducer from './reducers'
-
+import rootReducer from './reducers';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const loggerMiddleware = createLogger()
+window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
+
+const loggerMiddleware = createLogger();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
   {},
-  composeEnhancers(applyMiddleware(thunkMiddleware, loggerMiddleware)),
-)
+  composeEnhancers(applyMiddleware(thunkMiddleware, loggerMiddleware))
+);
 
 // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
@@ -29,7 +30,7 @@ render(
     <App />
   </Provider>,
   document.getElementById('root')
-)
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
